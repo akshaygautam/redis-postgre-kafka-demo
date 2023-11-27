1ffa1920c6a0514cf76d6e958093323ae9208236
akshaygautam <akshay.gautam2209@gmail.com>
2023-11-27 13:30:22 +0530

    first commit

---
+++ src/main/java/com/akshaygautam/redispostgrekafkademo/RedisPostgreKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.akshaygautam.redispostgrekafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPostgreKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisPostgreKafkaDemoApplication.class, args);
+	}
+
+}
